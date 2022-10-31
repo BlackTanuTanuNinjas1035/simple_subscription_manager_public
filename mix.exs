7735acb60d4febad4f735033480aaf1f6b08f571
BlackTanuTanuNinjas1035 <ickmkvo15@gmail.com>
2022-11-01 07:09:58 +0900

    タスクスケジュールするためにquantumライブラリを追加

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule SimpleSubscriptionManager.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:hackney, "~> 1.9"},
+      {:quantum, "~> 3.0"},
     ]
   end
 
