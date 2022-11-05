2bb0ec8e8c11c1e104d537777471a2793370fcf5
k10dayo <katoyorosiku213@gmail.com>
2022-11-05 21:06:20 +0900

    登録済みサブスク画面で更新日をスライドさせる

--- priv/static/assets/manager_index.js
+++ priv/static/assets/manager_index.js
@@ -1,20 +1,3 @@
-// $(function () {
-//     let ls_n = document.getElementsByClassName("subsc_name");
-//     let ls_p = document.getElementsByClassName("subsc_price");
-
-//     let ls_name = [];
-//     let ls_price = [];
-
-//     let tmp;
-//     for ( let t = 0; t < ls_n.length; t++) {
-//         console.log("hello");
-//         tmp = ls_n[t].textContent;
-//         ls_name.push(tmp.replace(/\s+/g, ''));  //空白と改行を取り除いてからpush
-//         tmp = ls_p[t].textContent;
-//         ls_price.push(tmp.replace(/\s+/g, ''));
-//         };
-//     console.log(ls_name, ls_price);
-// })
 
 function getSubsc() {
     let ls_n = document.getElementsByClassName("subsc_name");
@@ -40,4 +23,8 @@ $(function(){
     $(".subsc_option").on("click", function(){
         $(this).find(".subsc_option_menu").toggleClass("active");
     });
+    $(".change").on("click", function(){
+        $(this).parents(".subsc_card").children(".subsc_change_payment").slideToggle(300);
+        // $(this).parents(".subsc_card").children(".subsc_change_payment").toggleClass("active");
+    })
 })
