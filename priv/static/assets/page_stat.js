4e513dcb71cc614b43cd511b441e99a85833d064
k10dayo <katoyorosiku213@gmail.com>
2022-11-21 23:59:04 +0900

    統計画面の棒グラフの処理を作りました　がんばった　けどまだ値表示のプラグインの処理がみすってる

--- priv/static/assets/page_stat.js
+++ priv/static/assets/page_stat.js
@@ -9,12 +9,224 @@ $(function(){
         let selected = selected_button.data("tab");
         //console.log(selected);
         let selected_tab = ".ranking_" + selected;
-        console.log(selected_tab);
+        //console.log(selected_tab);
         $(".ranking").removeClass("active");
         $(selected_tab).addClass("active");
     })
 
     //olから値取る
-    let ol_class = $("#unko li:nth-child(1)").child(".subsc_reginum");
-    console.log(ol_class);
+    // let subsc_name_list = [];
+    // let subsc_reginum_list = [];
+    // let sum = 0;  //すべての合計件数
+    // let sum_less_than = 0; //11以下の合計件数
+    // let ol_selected = document.querySelector("#aaa");
+    // console.log(ol_selected);
+    // let li_list = ol_selected.querySelectorAll("li");
+
+    // for(let i = 0; i < li_list.length; i++) {
+    //     let tmp_name = li_list[i].querySelector(".subsc_name").textContent;
+    //     let tmp_reginum = parseFloat(li_list[i].querySelector(".subsc_reginum").textContent);
+
+    //     if (i < 10) {
+    //         //10未満の処理
+    //         subsc_name_list[i] = tmp_name;
+    //         subsc_reginum_list[i] = tmp_reginum;
+    //         sum += tmp_reginum;
+    //     } else if (i == 10) {
+    //         //10のときの処理
+    //         subsc_name_list[10] = "その他";
+    //         sum += tmp_reginum;
+    //         sum_less_than += tmp_reginum;
+    //     } else {
+    //         sum += tmp_reginum;
+    //         sum_less_than += tmp_reginum;
+    //     }
+    // }
+    // subsc_reginum_list.push(sum)
+    // console.log(subsc_name_list);
+    // console.log(subsc_reginum_list);
+    // console.log(sum);
+    // console.log(sum_less_than);
+
+    //get_ranking("#aaa")
+
 })
+
+//ol_rankingのtagdataを受け取るとspanの要素を取得し、リストにして返す
+function get_ranking(ranking_and_graph_id) {
+    //受取ったtag_dataはquerySelectorで取得したrankingのolのやつ
+    //olから値取る
+    let subsc_name_list = [];
+    let subsc_reginum_list = [];
+    let sum = 0;  //すべての合計件数
+    let sum_less_than = 0; //11以下の合計件数
+    //let ol_selected = document.querySelector(tag_data);
+
+    let a = document.querySelector("#" + ranking_and_graph_id);
+    let b = a.querySelector(".ol_ranking");
+
+    let li_list = b.querySelectorAll("li");
+
+    for(let i = 0; i < li_list.length; i++) {
+        let tmp_name = li_list[i].querySelector(".subsc_name").textContent;
+        let tmp_reginum = parseFloat(li_list[i].querySelector(".subsc_reginum").textContent);
+
+        if (i < 10) {
+            //10未満の処理
+            subsc_name_list[i] = tmp_name;
+            subsc_reginum_list[i] = tmp_reginum;
+            sum += tmp_reginum;
+        } else if (i == 10) {
+            //10のときの処理
+            subsc_name_list[10] = "その他";
+            sum += tmp_reginum;
+            sum_less_than += tmp_reginum;
+        } else {
+            sum += tmp_reginum;
+            sum_less_than += tmp_reginum;
+        }
+    }
+    subsc_reginum_list.push(sum_less_than)
+    // console.log(subsc_name_list);
+    // console.log(subsc_reginum_list);
+    // console.log(sum);
+    // console.log(sum_less_than);
+    re = [subsc_name_list, subsc_reginum_list];
+    return re
+}
+
+function get_ranking_genre(ranking_and_graph_id) {
+    //受取ったtag_dataはquerySelectorで取得したrankingのolのやつ
+    //olから値取る
+    let subsc_genre_list = [];
+    let subsc_reginum_list = [];
+    let sum = 0;  //すべての合計件数
+    let sum_less_than = 0; //11以下の合計件数
+    //let ol_selected = document.querySelector(tag_data);
+    let a = document.querySelector("#" + ranking_and_graph_id);
+    let b = a.querySelector(".ol_ranking");
+    let li_list = b.querySelectorAll("li");
+
+    for(let i = 0; i < li_list.length; i++) {
+        let tmp_genre = li_list[i].querySelector(".subsc_genre").textContent;
+        let tmp_reginum = parseFloat(li_list[i].querySelector(".subsc_reginum").textContent);
+
+        if (i < 10) {
+            //10未満の処理
+            subsc_genre_list[i] = tmp_genre;
+            subsc_reginum_list[i] = tmp_reginum;
+            sum += tmp_reginum;
+        } else if (i == 10) {
+            //10のときの処理
+            subsc_genre_list[10] = "その他";
+            sum += tmp_reginum;
+            sum_less_than += tmp_reginum;
+        } else {
+            sum += tmp_reginum;
+            sum_less_than += tmp_reginum;
+        }
+    }
+    subsc_reginum_list.push(sum_less_than)
+    // console.log(subsc_name_list);
+    // console.log(subsc_reginum_list);
+    // console.log(sum);
+    // console.log(sum_less_than);
+    re = [subsc_genre_list, subsc_reginum_list];
+    return re
+}
+
+
+//年代別ランキングを囲ったage_genreのidを渡すとchart.jsでつかうdatasetsにいれるリストが返ってくる
+function get_age_ranking_genre(age_genre_id){
+    let age_genre = document.querySelector(age_genre_id);
+    let age_genre_list = age_genre.querySelectorAll(".mini_ranking_and_graph"); 
+
+    //初期化
+     //年代の数　8個　ループするときに使う
+    let age_num = 8
+    //ジャンルのリスト
+    let genre_list = [ "動画配信のサービス", "音楽配信のサービス", "カーレンタルのサービス", "食品のサービス", "ソフトウェア提供のサービス","家具・家電のサービス","習い事や趣味のサービス","本のサービス"]
+    //age_data_listに8つの年代の辞書を格納する
+    let age_data_list = [];
+    for(let i = 0; i < age_num; i++) {
+        //年代の個数ずつ空辞書をpush
+        age_data_list.push({});
+        for(let j = 0; j < genre_list.length; j++) {
+            //すべてのジャンルの件数を0件にして初期化
+            age_data_list[i][genre_list[j]] = 0;
+        }
+    }
+    //console.log(age_data_list);
+
+
+    //年代のループ　10未満　10代　20代
+    for(let i = 0; i < age_num; i ++) {
+        let ranking_and_graph_id = age_genre_list[i].getAttribute("id");
+        //console.log(ranking_and_graph_id);
+        let ranking_data = get_ranking_genre(ranking_and_graph_id);
+        //特定の代のランキングのループ　例 30代のランキング　1.動画 2.音楽
+        for(let j = 0; j < ranking_data[0].length; j++) {
+            //console.log(ranking_data[0][j]);
+            //age_data_list年代ごとのリストに　ジャンルの件数を入力する
+            //ranking_data[0]はジャンルの名称がランキング順に格納されている
+            switch(ranking_data[0][j]) {
+                case genre_list[0]:
+                    age_data_list[i][genre_list[0]] = ranking_data[1][j];
+                    //console.log(age_data_list[i][genre_list[0]]);
+                    break;
+                case genre_list[1]:
+                    age_data_list[i][genre_list[1]] = ranking_data[1][j];
+                    break;
+                case genre_list[2]:
+                    age_data_list[i][genre_list[2]] = ranking_data[1][j];
+                    break;
+                case genre_list[3]:
+                    age_data_list[i][genre_list[3]] = ranking_data[1][j];
+                    break;
+                case genre_list[4]:
+                    age_data_list[i][genre_list[4]] = ranking_data[1][j];
+                    break;
+                case genre_list[5]:
+                    age_data_list[i][genre_list[5]] = ranking_data[1][j];
+                    break;
+                case genre_list[6]:
+                    age_data_list[i][genre_list[6]] = ranking_data[1][j];
+                    break;
+                case genre_list[7]:
+                    age_data_list[i][genre_list[7]] = ranking_data[1][j];
+                    break;
+            }
+        }
+        //console.log(age_data_list[i]);
+    }
+
+    //age_data_listのキーの数を数える　年代の数 age_numでもいいけど　変え忘れるといけないので
+    age_data_list_age_num = Object.keys(age_data_list).length;
+    re_age_data = []
+    for(let i = 0; i < genre_list.length; i++) {
+        re_age_data.push([]);
+    }
+    //年代と年代ごとジャンルの2重ループ　iが年代　jが年代ごとジャンル
+    for(let i = 0; i < age_data_list_age_num; i++) {
+        for(let j = 0; j < genre_list.length; j++) {
+            //age_data_listの該当する年代(age_data_list[i])のキーを指定(genre_list[j])してpushしてる
+            re_age_data[j].push(age_data_list[i][genre_list[j]]);
+        }
+    }
+    //ここのre_age_dataは　大枠　ジャンルごと　小枠　年代別になってる
+    //re_age_data[動画, 音楽, カー] ジャンルの並びはgenre_listの順
+    //動画[00代, 10代, 20代]
+    //console.log(re_age_data);
+
+    let color_palette = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "gray", "white"]
+    let re = [];
+    for(let i = 0; i < genre_list.length; i++) {
+        re.push({});
+        re[i]["label"] = genre_list[i];
+        re[i]["backgroundColor"] = color_palette[i];
+        re[i]["data"] = re_age_data[i]
+        //console.log(re[i]);
+    }
+    return re
+
+}
--- priv/static/assets/page_stat_graph.js
+++ priv/static/assets/page_stat_graph.js
@@ -1,78 +1,270 @@
 //値をグラフに表示させる
-Chart.plugins.register({
-    afterDatasetsDraw: function (chart, easing) {
-        var ctx = chart.ctx;
+// Chart.plugins.register({
+//     afterDatasetsDraw: function (chart, easing) {
+//         var ctx = chart.ctx;
 
-        chart.data.datasets.forEach(function (dataset, i) {
-            var meta = chart.getDatasetMeta(i);
-            if (!meta.hidden) {
-                meta.data.forEach(function (element, index) {
-                    // 値の表示
-                    ctx.fillStyle = 'rgb(0, 0, 0,0.8)';//文字の色
-                    var fontSize = 12;//フォントサイズ
-                    var fontStyle = 'normal';//フォントスタイル
-                    var fontFamily = 'Arial';//フォントファミリー
-                    ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+//         chart.data.datasets.forEach(function (dataset, i) {
+//             var meta = chart.getDatasetMeta(i);
+//             if (!meta.hidden) {
+//                 meta.data.forEach(function (element, index) {
+//                     // 値の表示
+//                     ctx.fillStyle = 'rgb(0, 0, 0,0.8)';//文字の色
+//                     var fontSize = 12;//フォントサイズ
+//                     var fontStyle = 'normal';//フォントスタイル
+//                     var fontFamily = 'Arial';//フォントファミリー
+//                     ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
 
-                    var dataString = dataset.data[index].toString();
+//                     //var dataString = dataset.data[index].toString();
+//                     //var dataString = chart.data.labels[index];
         
-                    // 値の位置
-                    ctx.textAlign = 'center';//テキストを中央寄せ
-                    ctx.textBaseline = 'middle';//テキストベースラインの位置を中央揃え
+//                     // 値の位置
+//                     ctx.textAlign = 'center';//テキストを中央寄せ
+//                     ctx.textBaseline = 'middle';//テキストベースラインの位置を中央揃え
 
-                    var padding = 5;//余白
-                    var position = element.tooltipPosition();
-                    ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
+//                     var padding = 5;//余白
+//                     var position = element.tooltipPosition();
+//                     ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
     
-                });
-            }
-        });
-    }
-});
+//                 });
+//             }
+//         });
+        
+//     }
+// });
 
-function graphView(graph_id) {
-        var ctx=document.getElementById(graph_id);//グラフを描画したい場所のid
+// function graphView(graph_id) {
+//         var ctx=document.getElementById(graph_id);//グラフを描画したい場所のid
 
-        // var tmp = getSubsc();
-        // var ls_name = tmp[0];
-        // var ls_price = tmp[1];
-        //console.log(ls_name);
-        var chart=new Chart(ctx,{
-            type:'pie',//グラフのタイプ
-            //type:'doughnut',
-            data:{//グラフのデータ
-                labels:["Abemaプレミアム", "Youtubeプレミアム"],//データの名前
-                //labels:ls_name,
-                datasets:[{
-                    label:"職種別比率",//グラフのタイトル
-                    backgroundColor:["#BB5179","#FAFF67", "#58A27C","#3C00FF"],//グラフの背景色
-                    data:["960", "1000"]//データ
-                    //data:ls_price
-                }]
+//         // var tmp = getSubsc();
+//         // var ls_name = tmp[0];
+//         // var ls_price = tmp[1];
+//         //console.log(ls_name);
+//         var chart=new Chart(ctx,{
+//             type:'pie',//グラフのタイプ
+//             //type:'doughnut',
+//             data:{//グラフのデータ
+//                 labels:["Abemaプレミアム", "Youtubeプレミアム"],//データの名前
+//                 //labels:ls_name,
+//                 datasets:[{
+//                     label:"職種別比率",//グラフのタイトル
+//                     backgroundColor:["#BB5179","#FAFF67", "#58A27C","#3C00FF"],//グラフの背景色
+//                     data:["960", "1000"]//データ
+//                     //data:ls_price
+//                 }]
+//             },
+
+//             options:{//グラフのオプション
+//                 maintainAspectRatio: false,//CSSで大きさを調整するため、自動縮小をさせない
+//                 legend:{
+//                     display:true//グラフの説明を表示
+//                 },
+//                 tooltips:{//グラフへカーソルを合わせた際の詳細表示の設定
+//                     callbacks:{
+//                         label: function (tooltipItem, data) {
+//                         return data.labels[tooltipItem.index]+ ": "+ data.datasets[0].data[tooltipItem.index] + "%";//%を最後につける
+//                         }
+//                     },    
+//                 },
+//                 title:{//上部タイトル表示の設定
+//                     display: true,
+//                     fontSize:10,
+//                     text: '単位：%'
+//                 },
+//             }
+            
+            
+//         });
+//         return chart
+// }
+
+//ranking_and_graphのところのidを引数で渡す
+function graphView(ranking_and_graph_id) {
+    let a = document.querySelector("#" + ranking_and_graph_id);
+    //let b = a.querySelector(".ol_ranking");
+    //let ranking_data = get_ranking(b);
+    let ranking_data = get_ranking(ranking_and_graph_id)
+    //console.log(ranking_data);
+    var ctx = a.querySelector(".graph"); //グラフを描画したい場所のid
+
+    var chart=new Chart(ctx,{
+        type:'doughnut',//グラフのタイプ
+        //type:'doughnut',
+        data:{//グラフのデータ
+            //labels:["Abemaプレミアム", "Youtubeプレミアム"],//データの名前
+            labels:ranking_data[0],
+            datasets:[{
+                label:"職種別比率",//グラフのタイトル
+                backgroundColor:["#BB5179","#FAFF67", "#58A27C","#3C00FF"],//グラフの背景色
+                //data:["960", "1000"]//データ
+                data:ranking_data[1]
+            }]
+        },
+
+        options:{//グラフのオプション
+            maintainAspectRatio: false,//CSSで大きさを調整するため、自動縮小をさせない
+            legend:{
+                display:false//グラフの説明を表示
+            },
+            tooltips:{//グラフへカーソルを合わせた際の詳細表示の設定
+                callbacks:{
+                    label: function (tooltipItem, data) {
+                    //return data.labels[tooltipItem.index]+ ": "+ data.datasets[0].data[tooltipItem.index] + "%";//%を最後につける
+                    return data.labels[tooltipItem.index]
+                    }
+                },    
+            },
+            title:{//上部タイトル表示の設定
+                display: false,
+                fontSize:10,
+                text: '単位：%'
             },
 
-            options:{//グラフのオプション
-                maintainAspectRatio: false,//CSSで大きさを調整するため、自動縮小をさせない
-                legend:{
-                    display:true//グラフの説明を表示
-                },
-                tooltips:{//グラフへカーソルを合わせた際の詳細表示の設定
-                    callbacks:{
-                        label: function (tooltipItem, data) {
-                        return data.labels[tooltipItem.index]+ ": "+ data.datasets[0].data[tooltipItem.index] + "%";//%を最後につける
-                        }
-                    },    
-                },
-                title:{//上部タイトル表示の設定
-                    display: true,
-                    fontSize:10,
-                    text: '単位：%'
-                },
+             //このpluginsはパーセンテージ表示の外部のやつ
+            plugins: {
+                labels: {
+                render: 'percentage',
+                fontColor: 'black',
+                fontSize: 20
+                }
             }
-            
-        });
-        return chart
+        }
+        
+    });
+    return chart
 }
+//ジャンル用のグラフ描画関数　liのspanのclassが違うから取ってくる関数が違うだけなはず
+function graphView_genre(ranking_and_graph_id) {
+    let a = document.querySelector("#" + ranking_and_graph_id);
+    //let b = a.querySelector(".ol_ranking");
+    //let ranking_data = get_ranking_genre(b);
+    let ranking_data = get_ranking_genre(ranking_and_graph_id);
+    //console.log(ranking_data);
+    var ctx = a.querySelector(".graph"); //グラフを描画したい場所のid
+
+    var chart=new Chart(ctx,{
+        type:'doughnut',//グラフのタイプ
+        //type:'doughnut',
+        data:{//グラフのデータ
+            //labels:["Abemaプレミアム", "Youtubeプレミアム"],//データの名前
+            labels:ranking_data[0],
+            datasets:[{
+                label:"職種別比率",//グラフのタイトル
+                backgroundColor:["#BB5179","#FAFF67", "#58A27C","#3C00FF"],//グラフの背景色
+                //data:["960", "1000"]//データ
+                data:ranking_data[1]
+            }]
+        },
+
+        options:{//グラフのオプション
+            maintainAspectRatio: false,//CSSで大きさを調整するため、自動縮小をさせない
+            legend:{
+                display:false//グラフの説明を表示
+            },
+            tooltips:{//グラフへカーソルを合わせた際の詳細表示の設定
+                callbacks:{
+                    label: function (tooltipItem, data) {
+                    //return data.labels[tooltipItem.index]+ ": "+ data.datasets[0].data[tooltipItem.index] + "%";//%を最後につける
+                    return data.labels[tooltipItem.index]
+                    }
+                },    
+            },
+            title:{//上部タイトル表示の設定
+                display: false,
+                fontSize:10,
+                text: '単位：%'
+            },
+
+             //このpluginsはパーセンテージ表示の外部のやつ
+            plugins: {
+                labels: {
+                render: 'percentage',
+                fontColor: 'black',
+                fontSize: 20
+                }
+            }
+        }
+        
+    });
+    return chart
+}
+
+function graphView_age_genre(age_genre_id) {
+    let a = document.querySelector("#" + age_genre_id);
+    let datasets_data = get_age_ranking_genre("#" + age_genre_id);
+    console.log(datasets_data);
+    var ctx = a.querySelector(".bar_graph"); //グラフを描画したい場所のid
+
+    var chart=new Chart(ctx,{
+        type:'bar',//グラフのタイプ
+        //type:'doughnut',
+        data:{//グラフのデータ
+            labels:["00代", "10代", "20代", "30代", "40代", "50代", "60代", "70代"],//データの名前
+            //labels:ranking_data[0],
+            // datasets: [
+            //     {
+            //         label:"動画配信のサービス",//グラフのタイトル
+            //         //backgroundColor:["#BB5179","#FAFF67", "#58A27C","#3C00FF"],//グラフの背景色
+            //         backgroundColor: "red",
+            //         data:[100, 10, 100]//データ
+            //         //data:ranking_data[1]
+            //     },
+            //     {
+            //         label:"音楽配信のサービス",//グラフのタイトル
+            //         //backgroundColor:["#3C00FF","#58A27C", "#FAFF67","BB5179"],//グラフの背景色
+            //         backgroundColor: "blue",
+            //         data:[200, 200, 20]//データ
+            //     }
+            // ]
+            datasets: datasets_data
+        },
+
+        options:{//グラフのオプション
+            maintainAspectRatio: false,//CSSで大きさを調整するため、自動縮小をさせない
+            legend:{
+                display:false//グラフの説明を表示
+            },
+            tooltips:{//グラフへカーソルを合わせた際の詳細表示の設定
+                callbacks:{
+                    label: function (tooltipItem, data) {
+                    //return data.labels[tooltipItem.index]+ ": "+ data.datasets[0].data[tooltipItem.index] + "%";//%を最後につける
+                    return data.datasets[tooltipItem.datasetIndex].label
+                    }
+                },    
+            },
+            scales: {
+                xAxes: [
+                    {
+                        stacked: true  // 積み上げの指定
+                    }
+                ],
+                yAxes: [
+                    {
+                        stacked: true  //  積み上げの指定
+                    }
+                ]
+            },
+            title:{//上部タイトル表示の設定
+                display: false,
+                fontSize:10,
+                text: '単位：%'
+            },
+
+            //このpluginsはパーセンテージ表示の外部のやつ
+            plugins: {
+                labels: {
+                render: 'percentage',
+                fontColor: 'black',
+                fontSize: 20,
+                position: "default"
+                }
+            }
+        }
+        
+    });
+    return chart
+
+}
+
 //確認用
 function hello(){
     console.log("hello");
@@ -91,25 +283,27 @@ $(function() {
         flaglist_graph_male_age_service[i] = true;
     }
 
-    //女性のサービスランキング
-    let graph_female_service = true;
-    //女性の年齢別のサービスランキング
+    //女性のサービスランキングフラグ
+    let flag_graph_female_service = true;
+    //女性の年齢別のサービスランキングフラグ
     let flaglist_graph_female_age_service = [];
     for (let i = 0; i < age_number; i++) {
         flaglist_graph_female_age_service[i] = true;
     }
 
+    //すべてのサービスランキングのグラフ描画処理
     $("#graph_overall_service").on("inview", function() {
         if (flag_graph_overall_service) {
             flag_graph_overall_service = false;
-            let tmp = "graph_overall_service";
+            let tmp = "overall_service";
             let chart_graph_overall_service = graphView(tmp); //オブジェクトが返ってくる　個別にoptionなど変更したいとき
         };
     });
-    $("#graph_male_service").on("inview", function() {
+    //すべての男性のサービスグラフランキングの描画処理
+    $("#graph_male_overall_service").on("inview", function() {
         if (flag_graph_male_service) {
             flag_graph_male_service = false;
-            let tmp = "graph_male_service";
+            let tmp = "male_overall_service";
             let  chart_graph_male_service = graphView(tmp); //オブジェクトが返ってくる　個別にoptionなど変更したいとき
         };
     });
@@ -118,7 +312,7 @@ $(function() {
     let chart_id;
     let chart_id_sub; //#つけたchart_id
     for(let i = 0; i < age_number; i++) {
-        chart_id = "graph_male_age_" + i + "0_service";
+        chart_id = "male_age_" + i + "0_service";
         chart_id_sub = "#" + chart_id;
         //即時関数　(function(渡す変数)){}(受け取る変数名)
         (function(i, chart_id, chart_id_sub, flaglist_graph_male_age_service, chartlist_graph_male_age_service){
@@ -130,9 +324,18 @@ $(function() {
             })
         })(i, chart_id, chart_id_sub, flaglist_graph_male_age_service, chartlist_graph_male_age_service);
     }
+    //女性
+    $("#graph_female_service").on("inview", function() {
+        if (flag_graph_female_service) {
+            flag_graph_female_service = false;
+            let tmp = "female_overall_service";
+            let  chart_graph_male_service = graphView(tmp); //オブジェクトが返ってくる　個別にoptionなど変更したいとき
+        };
+    });
+    //女性年齢別
     let chartlist_graph_female_age_service = [];
     for(let i = 0; i < age_number; i++) {
-        chart_id = "graph_female_age_" + i + "0_service";
+        chart_id = "female_age_" + i + "0_service";
         chart_id_sub = "#" + chart_id;
         (function(i, chart_id, chart_id_sub, flaglist_graph_female_age_service, chartlist_graph_female_age_service){
             $(chart_id_sub).on("inview", function() {
@@ -144,5 +347,66 @@ $(function() {
         })(i, chart_id, chart_id_sub, flaglist_graph_female_age_service, chartlist_graph_female_age_service);
     }
 
+    //ジャンルコーナー
+
+    //すべてのジャンルランキングフラグ↓
+    let flag_graph_overall_genre = true;
+    //男性のジャンルランキングフラグ
+    let flag_graph_male_genre = true;
+    //男性の年齢別のジャンルランキングフラグ
+    let flag_graph_male_age_genre = true;
+
+    //女性のジャンルランキングフラグ
+    let flag_graph_female_genre = true;
+    //女性の年齢別のジャンルランキングフラグ
+    let flag_graph_female_age_genre = true;
+
+    $("#graph_overall_genre").on("inview", function() {
+        if (flag_graph_overall_genre) {
+            flag_graph_overall_genre = false;
+            let tmp = "overall_genre";
+            let  chart_graph_male_genre = graphView_genre(tmp); //オブジェクトが返ってくる　個別にoptionなど変更したいとき
+        };
+    });
+    $("#graph_male_overall_genre").on("inview", function() {
+        if (flag_graph_male_genre) {
+            flag_graph_male_genre = false;
+            let tmp = "male_overall_genre";
+            let  chart_graph_male_genre = graphView_genre(tmp); //オブジェクトが返ってくる　個別にoptionなど変更したいとき
+        };
+    });
+
+
+    $("#graph_female_overall_genre").on("inview", function() {
+        if (flag_graph_female_genre) {
+            flag_graph_female_genre = false;
+            let tmp = "female_overall_genre";
+            let  chart_graph_male_genre = graphView_genre(tmp); //オブジェクトが返ってくる　個別にoptionなど変更したいとき
+        };
+    });
+
+    //棒グラフ
+    let flag_bar_graph_age_male_genre = true;
+    let flag_bar_graph_age_female_genre = true;
+
+
+
+
+    $("#bar_graph_male_genre").on("inview", function() {
+        if(flag_bar_graph_age_male_genre) {
+            flag_bar_graph_age_male_genre = false;
+            let tmp = "male_age_genre";
+            let chart_bar_graph_age_male_genre = graphView_age_genre(tmp);
+        }
+        
+    });
+    $("#bar_graph_female_genre").on("inview", function() {
+        if(flag_bar_graph_age_female_genre) {
+            flag_bar_graph_age_female_genre = false;
+            let tmp = "female_age_genre";
+            let chart_bar_graph_age_female_genre = graphView_age_genre(tmp);
+        }
+        
+    });
 
 });
