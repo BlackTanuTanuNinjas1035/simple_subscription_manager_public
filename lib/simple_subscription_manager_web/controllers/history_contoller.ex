c8eb6b7a87a93991dba38e50eac5c32f4e697550
k10dayo <katoyorosiku213@gmail.com>
2022-12-06 09:41:09 +0900

    履歴作ってる途中マージ前

--- lib/simple_subscription_manager_web/controllers/history_contoller.ex
+++ lib/simple_subscription_manager_web/controllers/history_contoller.ex
@@ -1,7 +1,7 @@
 defmodule SimpleSubscriptionManagerWeb.HistoryController do
   use SimpleSubscriptionManagerWeb, :controller
 
-  alias SimpleSubscriptionManager.History
+  alias SimpleSubscriptionManager.Historys
 
   # ユーザの履歴を表示する
   def index(conn, _params) do
@@ -9,18 +9,18 @@ defmodule SimpleSubscriptionManagerWeb.HistoryController do
     account_id = conn.assigns[:current_account].id
 
     render(conn, "history.html",
-    history_month_1: History.get_history(account_id, 1),
-    history_month_2: History.get_history(account_id, 2),
-    history_month_3: History.get_history(account_id, 3),
-    history_month_4: History.get_history(account_id, 4),
-    history_month_5: History.get_history(account_id, 5),
-    history_month_6: History.get_history(account_id, 6),
-    history_month_7: History.get_history(account_id, 7),
-    history_month_8: History.get_history(account_id, 8),
-    history_month_9: History.get_history(account_id, 9),
-    history_month_10: History.get_history(account_id, 10),
-    history_month_11: History.get_history(account_id, 11),
-    history_month_12: History.get_history(account_id, 12)
+    history_month_1: Historys.get_history(account_id, 1),
+    history_month_2: Historys.get_history(account_id, 2),
+    history_month_3: Historys.get_history(account_id, 3),
+    history_month_4: Historys.get_history(account_id, 4),
+    history_month_5: Historys.get_history(account_id, 5),
+    history_month_6: Historys.get_history(account_id, 6),
+    history_month_7: Historys.get_history(account_id, 7),
+    history_month_8: Historys.get_history(account_id, 8),
+    history_month_9: Historys.get_history(account_id, 9),
+    history_month_10: Historys.get_history(account_id, 10),
+    history_month_11: Historys.get_history(account_id, 11),
+    history_month_12: Historys.get_history(account_id, 12)
       )
   end
 
