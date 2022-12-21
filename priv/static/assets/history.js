da4d057625bb88cef754d76d5c31b2f22dc6c115
k10dayo <katoyorosiku213@gmail.com>
2022-12-22 04:08:44 +0900

    スクロール矢印など作りました

--- priv/static/assets/history.js
+++ priv/static/assets/history.js
@@ -1,28 +1,13 @@
 $(function() {
-    
-    // let data_list = document.querySelectorAll(".data");
-    // let sum_list = [];
-    // //monthは文字列のリスト
-    // let month_list = []
-
-    // for(let i = 0; i < data_list.length; i++) {
-    //     let data = data_list[i];
-    //     let month = data.querySelector(".span_month").textContent;
-    //     month_list.push(month);
-    //     let card_list = data.querySelectorAll(".data_card");
-    //     let price_sum = 0;
-    //     for(let j = 0; j < card_list.length; j++) {
-    //         price_sum += parseInt(card_list[j].querySelector(".price").textContent);
-    //     }
-    //     sum_list.push(price_sum);
-    // }
-    // console.log(sum_list);
-    // console.log(month_list);
-
-    // let return_list = [];
-    // return_list.push(month_list.reverse())
-    // return_list.push(sum_list.reverse())
-    // console.log(return_list[0]);
+    let scroll_parent = document.querySelector("#history_flame_in");
+    $("#arrow_left").on('click', function() {
+        let scroll_width = 0 - document.querySelector("#before_1").clientWidth;
+        scroll_parent.scrollBy( scroll_width, 0);
+    });
+    $("#arrow_right").on('click', function() {
+        let scroll_width = document.querySelector("#before_1").clientWidth;
+        scroll_parent.scrollBy( scroll_width, 0);
+    });
 });
 
 function get_history_data() {
--- priv/static/assets/page_stat.js
+++ priv/static/assets/page_stat.js
@@ -13,6 +13,39 @@ $(function(){
         $(".ranking").removeClass("active");
         $(selected_tab).addClass("active");
     });
+
+    //スクロール矢印の処理
+    $(".arrow_right_flame").on("click", function() {
+        let parent = $(this).parent();
+        let scroll_width = parent[0].clientWidth;
+        let parent_id = "#" + parent.parent()[0].id;
+        let scroll_parent = document.querySelector(parent_id);
+        scroll_parent.scrollBy( scroll_width, 0);
+    });
+    $(".arrow_left_flame").on("click", function() {
+        let parent = $(this).parent();
+        let scroll_width = 0 - parent[0].clientWidth;
+        let parent_id = "#" + parent.parent()[0].id;
+        let scroll_parent = document.querySelector(parent_id);
+        scroll_parent.scrollBy( scroll_width, 0);
+    });
+
+    $(".arrow_left_flame_age").on("click", function() {
+        let parent_parent = $(this).parent();
+        let parent = parent_parent[0].querySelector(".age_circle_graph");
+        let parent_id = "#" + parent.id;
+        let scroll_parent = parent_parent[0].querySelector(parent_id);
+        let scroll_width = 0 - parent.querySelector(".mini_ranking_and_graph").clientWidth;
+        scroll_parent.scrollBy( scroll_width, 0);
+    });
+    $(".arrow_right_flame_age").on("click", function() {
+        let parent_parent = $(this).parent();
+        let parent = parent_parent[0].querySelector(".age_circle_graph");
+        let parent_id = "#" + parent.id;
+        let scroll_parent = parent_parent[0].querySelector(parent_id);
+        let scroll_width = parent.querySelector(".mini_ranking_and_graph").clientWidth;
+        scroll_parent.scrollBy( scroll_width, 0);
+    });
 });
 
 //ol_rankingのtagdataを受け取るとspanの要素を取得し、リストにして返す　あと取得するランキングの数を取得する
